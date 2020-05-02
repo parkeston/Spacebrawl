95ec7e9881adc2fb87fc8ced93e2232e78142276
parkeston <pasha.shpilevskiy@gmail.com>
2020-05-02 19:28:29 +0300

    Simple camera follower script added

---
+++ Assets/Systems/Locomotion/Scripts/Follower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField] private Transform follow;
+
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position-follow.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = follow.position + offset;
+    }
+}
