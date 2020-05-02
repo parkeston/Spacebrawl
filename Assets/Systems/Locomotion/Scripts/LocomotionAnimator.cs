122394b08348d9c871f3467e04c7f75386e8f6e3
parkeston <pasha.shpilevskiy@gmail.com>
2020-05-02 17:16:58 +0300

    Locomotion animator script added

---
+++ Assets/Systems/Locomotion/Scripts/LocomotionAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class LocomotionAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator locomotionAnimator;
+    [SerializeField] private InputReader inputReader;
+
+
+    //how to enforce specifying animator with the same interface(set of params, states, transitions & blend trees)
+
+    private float horizontalMotion;
+    private float verticalMotion;
+    
+    private static readonly int HorizontalMotion = Animator.StringToHash("horizontalMotion");
+    private static readonly int VerticalMotion = Animator.StringToHash("verticalMotion");
+
+    private void Update()
+    {
+        //converting world space movement direction to local space direction, for properly setting animation
+        Vector3 animationDirection = transform.InverseTransformDirection(inputReader.GetMovementDirection());
+        horizontalMotion = animationDirection.x;
+        verticalMotion = animationDirection.z;
+    }
+
+    private void FixedUpdate()
+    {
+        locomotionAnimator.SetFloat(HorizontalMotion,horizontalMotion);
+        locomotionAnimator.SetFloat(VerticalMotion,verticalMotion);
+    }
+}
