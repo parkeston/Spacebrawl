66919d8b1acfa69c18d943f429c5918da92a544f
parkeston <pasha.shpilevskiy@gmail.com>
2020-05-03 20:07:17 +0300

    Basic stats system added

---
+++ Assets/Systems/Stats/Scripts/ResourceAffectorObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ResourceAffectorObject : MonoBehaviour
+{
+    [SerializeField] private ResourceAffector[] affectors;
+    [SerializeField] private bool isTrigger;
+    [SerializeField] private bool destroyOnAffect;
+
+    private Collider collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider>();
+        collider.isTrigger = isTrigger;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        AffectOnCollision(other.gameObject.GetComponents<IResource>());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AffectOnCollision(other.GetComponents<IResource>());
+    }
+
+    private void AffectOnCollision(IResource[] resources)
+    {
+        if(resources==null || resources.Length==0)
+            return;
+        
+        foreach (var affector in affectors)
+        {
+            affector.Affect(resources);
+        }
+        
+        if(destroyOnAffect)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Systems/Stats/Scripts/Stat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[Serializable]
+public class Stat: MonoBehaviour, IResource, IConsumable
+{
+    [SerializeField] private ResourceType resourceType;
+    [SerializeField] private float maxValue;
+    [SerializeField] private float startingValue;
+
+    private float value;
+    
+    public ResourceType ResourceType => resourceType;
+    public float ResourceValue => value;
+
+    private void Awake()
+    {
+        value = startingValue;
+    }
+
+    private void OnValidate()
+    {
+        if (startingValue > maxValue)
+            startingValue = maxValue;
+    }
+
+    public void AlterResourceAmount(float alterAmount)
+    {
+        value += alterAmount;
+        if (value > maxValue)
+            value = maxValue;
+    }
+
+    public bool Consume(float consumeAmount)
+    {
+        if (consumeAmount > value)
+            return false;
+
+        value -= consumeAmount;
+        return true;
+    }
+}
+
