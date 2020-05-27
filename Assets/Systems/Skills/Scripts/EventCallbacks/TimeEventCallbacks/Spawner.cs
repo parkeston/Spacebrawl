77b15eeb5b4c9d42f98485166e0464b63fc4e87b
parkeston <pasha.shpilevskiy@gmail.com>
2020-05-27 10:13:13 +0300

    character death effects

---
+++ Assets/Systems/Skills/Scripts/EventCallbacks/TimeEventCallbacks/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private bool alignRotation;
+    [SerializeField] private GameObject spawnPrefab;
+    [SerializeField] private GameObject parentTo;
+
+    private GameObject spawnedObject;
+
+    //todo: use pool?
+    private  void Awake()
+    {
+        spawnedObject = Instantiate(spawnPrefab);
+        spawnedObject.transform.position = transform.position;
+        spawnedObject.SetActive(false);
+        if(parentTo!=null)
+            spawnedObject.transform.SetParent(parentTo.transform);
+        
+        if(spawnedObject.TryGetComponent(out Collider collider))
+            Physics.IgnoreCollision(collider,spawnedObject.GetComponent<Collider>());
+    }
+    
+    public void Spawn()
+    {
+        spawnedObject.transform.position = transform.position;
+        if(alignRotation)
+            spawnedObject.transform.rotation = transform.rotation;
+        spawnedObject.SetActive(true);
+    }
+}
